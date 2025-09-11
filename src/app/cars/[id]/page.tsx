0df58a0790df5a27e7b5c019534176cc755ca132
Chihiro88G <1114chihiro@gmail.com>
2025-09-11 11:12:30 -0400

    call client component on server component

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -1,14 +1,26 @@
 // for dynamic routes. [id]/page.tsx
 // this is already SSG under app folder
 
+import Button from "./button";
+
 type CarProps = {
   params: { id: string };
 };
 
+// able to make the component async
 export default async function Car({ params }: CarProps) {
-  const { id } = await params;  // need to do this in newer version. params have to be awaited
+  // need to do this in newer version. params have to be awaited
+  const { id } = await params;
+  
+  // able to fetch data without useEffect
   const req = await fetch(`http://localhost:3000/${id}.json`);
   const car = await req.json();
 
-  return <h1>SALUT {car.id} with {car.color} made in {car.made}</h1>;
+  return (
+    <>
+      {/* calling client component here from this server component */}
+      <Button />
+      <h1>SALUT {car.id} with {car.color} made in {car.made}</h1>
+    </>
+  );
 }
