c0e97b670486de7e31be0bbb98a324a9af1b1381
Chihiro88G <1114chihiro@gmail.com>
2025-09-11 18:30:17 -0400

    make backend and api call

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,5 +1,23 @@
 // use page.tsx (not index.tsx) as file name
 
-export default function CarsList() {
-  return <h1>CARS LIST</h1>
+async function makePostRequest() {
+
+  // make api call from server -> need a full uri
+  // make api call from client -> need only 'api/hello'
+  const res = await fetch('http://localhost:3000/api/hello', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name: 'Lucy' }),
+  });
+
+  const data = await res.json();
+  return { data };
+}
+
+export default async function CarsList() {
+  const { data } = await makePostRequest();
+
+  return <h1>{ data.message }, here is your CARS LIST</h1>
 }
